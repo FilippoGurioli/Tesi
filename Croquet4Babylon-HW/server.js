10d8d3634da31f4592250c94f842d95894a5b314
Filippo Gurioli <filippo.gurioli@studio.unibo.it>
2023-08-05 15:29:47 +0200

    Generalized the ip in server.js

--- Croquet4Babylon-HW/server.js
+++ Croquet4Babylon-HW/server.js
@@ -1,10 +1,26 @@
+const os = require('os');
+
+const networkInterfaces = os.networkInterfaces();
+const ipv4Addresses = [];
+
+for (const interfaceName in networkInterfaces) {
+    const interfaces = networkInterfaces[interfaceName];
+    for (const iface of interfaces) {
+        if (iface.family === 'IPv4' && !iface.internal) {
+            ipv4Addresses.push(iface.address);
+        }
+    }
+}
+
+console.log('Indirizzi IP locale:', ipv4Addresses);
+
 const https = require("https");
 const fs = require("fs");
 const express = require("express");
 var path = require("path");
 const app = express();
 const port = 3000;
-const host = '192.168.178.128'
+const host = ipv4Addresses[0];
 
 https.createServer(
 	{
@@ -15,9 +31,10 @@ https.createServer(
 	.listen(port, host, () => {
 		console.log('Server started at https://' + host + ':' + port);
 	});
-
-app.use(express.static(path.join(__dirname)));
-
-app.get("/", function (req, res) {
-	res.sendFile(path.join(__dirname, '/HelloWorld.html'));
-})
+	
+	app.use(express.static(path.join(__dirname)));
+	
+	app.get("/", function (req, res) {
+		res.sendFile(path.join(__dirname, '/HelloWorld.html'));
+	})
+	
