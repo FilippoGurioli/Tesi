a959b4979c4f409be0aea53a36c9eff3745488ad
FilippoGurioli <93600198+FilippoGurioli@users.noreply.github.com>
2023-08-08 20:00:10 +0200

    Set BattleField and Card model, wip

---
+++ Progetto/main/Model/BattleField.js
@@ -0,0 +1,23 @@
+class BattleField {
+    
+    p1MonsterField = [null, null, null, null, null];
+    p2MonsterField = [null, null, null, null, null];
+    p1SpellField = [null, null, null, null, null];
+    p2SpellField = [null, null, null, null, null];
+
+    placeMonster(card, player, position) {
+        if (player === 1) {
+            this.p1MonsterField[position] = card;
+        } else {
+            this.p2MonsterField[position] = card;
+        }
+    }
+
+    placeSpell(card, player, position) {
+        if (player === 1) {
+            this.p1SpellField[position] = card;
+        } else {
+            this.p2SpellField[position] = card;
+        }
+    }
+}
